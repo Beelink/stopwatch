02306ffbfc52c5909395bb3a8e92d4fa65ada217
Eugene Volynko <volynko.ua@gmail.com>
2021-11-09 16:25:28 +0200

    add Stopwatch content

--- src/components/Stopwatch/index.js
+++ src/components/Stopwatch/index.js
@@ -1,15 +1,32 @@
-import './index.scss';
+import "./index.scss";
 
-function Stopwatch({ id, value, play, valueChanged, playChanged }) {
+function Stopwatch({ value, play, playChanged }) {
+  const _onChange = () => {
+    playChanged(!play);
+  };
+
+  const _formatValue = (value) => {
+    let secNum = parseInt(value, 10);
+    let hours   = Math.floor(secNum / 3600);
+    let minutes = Math.floor((secNum - (hours * 3600)) / 60);
+    let seconds = secNum - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+
+    return hours+':'+minutes+':'+seconds;
+  };
+
   return (
     <div className="stopwatch">
+      <h3 className="stopwatch__value">{ _formatValue(value) }</h3>
       <ul className="stopwatch__buttons">
         <li>
-          { play ? (
-            <button className="stopwatch__button">Play</button>
-          ) : (
-            <button className="stopwatch__button">Play</button>
-          ) }
+          <button className="stopwatch__button" onClick={_onChange}>
+            {play ? <span>Pause</span> : <span>Play</span>}
+          </button>
+          <button className="stopwatch__button">Reset</button>
         </li>
       </ul>
     </div>
